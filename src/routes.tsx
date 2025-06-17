309af7e56a67892f57bc7d79b142fc882ee7106d
z-ph <you@example.com>
2025-06-17 22:28:42 +0800

    "重构路由配置，使用动态导入和循环简化路由定义"

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,37 +1,30 @@
 import { lazy } from 'react'
 import type { RouteObject } from 'react-router-dom'
-
-// import HomePage from './pages/HomePage'
-// import EnrollPage from './pages/EnrollPage'
-const HomePage = lazy(() => import('./pages/HomePage'))
-const EnrollPage = lazy(() => import('./pages/EnrollPage'))
-const ManagerPage = lazy(() => import('./pages/ManagerPage'))
-const OrderPage = lazy(() => import('./pages/OrderPage'))
-const ErrorPage = lazy(() => import('./pages/ErrorPage'))
-const ClassSelectPage = lazy(() => import('./pages/ClassSelectPage'))
-export const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/enroll",
-    element: <EnrollPage />,
-  },
-  {
-    path: "/classselect",
-    element: <ClassSelectPage />,
-  },
-  {
-    path: "/manager",
-    element: <ManagerPage />,
-  },
-  {
-    path: "/order",
-    element: <OrderPage />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />
-  },
-];
+import type { ComponentType } from 'react'
+const pages = ["","Home", "Enroll", "Manager", "Order", "ClassSelect","Error", '*'] as const;
+type Page = typeof pages[number];
+const pageComponents = import.meta.glob("./pages/*Page.tsx");
+function lazyImport(pageName: Page): ComponentType {
+  const componentPath = `./pages/${pageName}Page.tsx`;
+  return lazy(
+    pageComponents[componentPath] as () => Promise<{ default: ComponentType }>
+  );
+}
+function routesAdd(pageName: Page,Page: ComponentType) {
+  if(pageName === '*'){
+    Page = lazyImport('Error')
+  }
+  if(pageName === ''){
+    Page = lazyImport('Home')
+  }
+  routes.push({
+    path: `/${pageName}`,
+    element: <Page />,
+  });
+}
+const routes: RouteObject[] = [];
+pages.forEach(page => {
+  const Page = lazyImport(page);
+  routesAdd(page, Page);
+});
+export { routes }
